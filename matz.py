58d4da0563f931cf5a05a07c5ba0e66f45341497
Andrej T <andrejtrajchevski@gmail.com>
2012-09-21 11:43:14 +0200

    Removed 'requests' dependency. Refractoring.

--- matz.py
+++ matz.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 # url https://gist.github.com/raw/3756905
 def add(a,b):
-    return a + b + 100
+    return a + b + 200
 
 def subtract(a,b):
     return a - b
--- synchronize.py
+++ synchronize.py
@@ -1,42 +1,47 @@
-import requests
+import urllib2
 import os
 import inspect as i
 import codecs
+import logging
 
-# TODO:
-# - remove requests dependency
 
-# Get the location of the file that imports 'synchronize'
-# or use the current file location
-if __name__ != "__main__":
-    called_from = i.getframeinfo(i.getouterframes(i.currentframe())[1][0])[0]
-else:
+# Get the location of the file that imports 'synchronize'.
+# If the file wasn't imported, use the path of the current
+# file.
+if __name__ == "__main__":
     called_from = os.path.abspath(__file__)
+else:
+    called_from = i.getframeinfo(i.getouterframes(i.currentframe())[1][0])[0]
 
 
 def get_files_for_sync():
-    """ Walk the directory where the file is located and return a list of
-    files that need to be synced.
+    """ Walks the directory where the file is located and returns a list of
+    files that need to be synchronized.
     """
     current_dir = os.path.dirname(called_from)
-    possible_files_for_sync = []
+    dir_paths = []
     for folder in os.walk(current_dir):
         for path in folder[2]:  # folder[2] is a list of files in the folder
-            possible_files_for_sync.append(os.path.join(folder[0], path))
+            dir_paths.append(os.path.join(folder[0], path))
     files_for_sync = []
-    for path in possible_files_for_sync:
+    for path in dir_paths:
         with open(path) as f:
             for line in f.readlines():
-                if line.startswith("# url "):
-                    files_for_sync.append(path)
+                if not line.startswith("# url "):
                     break
+                files_for_sync.append(path)
     return files_for_sync
 
 
 def read_source(url):
     """ Read the source from the given URL.
     """
-    return requests.get(str(url)).text
+    try:
+        return urllib2.urlopen(str(url)).read()
+    except urllib2.URLError, e:
+        print "There was a error when fetching '%s':" % url
+        print e
+        print "Corresponding file won't be synchronized."
 
 
 def extract_url(text):
@@ -55,7 +60,11 @@ def update_file(file):
         local_source = f.read()
     url = extract_url(local_source)
     remote_source = read_source(url)
-    print "Fetched " + url
+    if not remote_source:
+        print "Could not load " + url
+        return
+    else:
+        print "Fetched " + url
     if local_source != remote_source:
         with codecs.open(str(file), 'w', 'utf-8') as f:
             f.write("# url %s\n" % url)
@@ -67,18 +76,20 @@ for filename in get_files_for_sync():
     update_file(filename)
 
 
-# Clean up the namespace
+# Clean up the namespace:
+#
+# Imports:
 del(i)
 del(os)
-del(requests)
+del(urllib2)
 del(codecs)
+del(logging)
 
+# Variables
+del(called_from)
+
+# Functions
 del(get_files_for_sync)
 del(read_source)
 del(extract_url)
 del(update_file)
-
-del(called_from)
-del(filename)
-
-print dir()
