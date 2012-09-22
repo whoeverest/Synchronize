28d6d31e250ac12d2bf12c346afa2ea4dbfcd1eb
Andrej T <andrejtrajchevski@gmail.com>
2012-09-22 23:36:33 +0200

    removed examples

--- main.py
+++
@@ -1,5 +0,0 @@
-import synchronize
-
-import matz
-
-print matz.add(2, 3)
--- matz.py
+++
@@ -1,6 +0,0 @@
-# url https://gist.github.com/raw/3756905
-def add(a,b):
-    return a + b + 500
-
-def subtract(a,b):
-    return a - b
