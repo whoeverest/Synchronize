4aa5b454afe0a9466b3596f11c2d2d50584c2f62
Andrej T <andrejtrajchevski@gmail.com>
2012-09-23 00:43:36 +0200

    Made URL calls parallel.

--- synchronize.py
+++ synchronize.py
@@ -2,6 +2,7 @@ import urllib2
 import os
 import inspect as i
 import codecs
+from multiprocessing import Process
 
 # TODO:
 # - Replace print statements with logging
@@ -35,12 +36,9 @@ def extract_url(file):
     """ Extract URL from comments, if any.
     """
     with open(file) as f:
-        while True:
-            line = f.readline()
-            if not line:
-                break
-            if line.startswith("# url "):
-                return line.split()[2]
+        line = f.readline()
+        if line.startswith("# url "):
+            return line.split()[2]
 
 
 def update_file(file):
@@ -55,6 +53,7 @@ def update_file(file):
     with codecs.open(str(file), 'w', 'utf-8') as f:
         f.write("# url %s\n" % url)
         f.write(remote_source)
+    print "Updated %s" % os.path.basename(file)
     return True
 
 
@@ -65,10 +64,13 @@ if __name__ == "__main__":
 else:
     called_from = i.getframeinfo(i.getouterframes(i.currentframe())[1][0])[0]
 
-# Slow and synchronous
+processes = []
 for filename in get_files(called_from):
-    if update_file(filename):
-        print "Synchronized %s (%s)" % (os.path.basename(filename), filename)
+    p = Process(target=update_file, args=(filename,))
+    p.start()
+    processes.append(p)
+for p in processes:
+    p.join()
 
 
 # Clean up the namespace...
@@ -77,6 +79,7 @@ del(i)
 del(os)
 del(urllib2)
 del(codecs)
+del(Process)
 
 # Variables
 del(called_from)
@@ -87,3 +90,7 @@ del(get_files)
 del(read_remote)
 del(extract_url)
 del(update_file)
+del(processes)
+del(p)
+
+print dir()
